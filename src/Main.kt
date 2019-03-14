6dc28e28eb2f2f5a3b31488b87e3df1b0fbb4fe7
asteinme <andrew@plusmobileapps.com>
2019-03-14 01:56:38 -0700

    implement median sort for two sorted arrays

--- src/Main.kt
+++ src/Main.kt
@@ -1,110 +1,114 @@
-
-import java.util.HashSet
-import java.util.HashMap
-
-
-
-
 /**
-Given a string, find the length of the longest substring without repeating characters.
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
 
 Example 1:
 
-Input: "abcabcbb"
-Output: 3
-Explanation: The answer is "abc", with the length of 3.
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
 Example 2:
 
-Input: "bbbbb"
-Output: 1
-Explanation: The answer is "b", with the length of 1.
-Example 3:
+nums1 = [1, 2]
+nums2 = [3, 4]
 
-Input: "pwwkew"
-Output: 3
-Explanation: The answer is "wke", with the length of 3.
-Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+The median is (2 + 3)/2 = 2.5
  */
 
-
 fun main(args: Array<String>) {
-    val strings = arrayListOf("abcabcbb", "bbbbb", "pwwkew")
+    val nums1 = intArrayOf(1,3)
+    val nums2 = intArrayOf(2)
 
-    printLongestSubstring(strings = strings, method = "All Unique") { string ->
-        lengthOfLongestSubstring(string)
+    printMedianOfTwoSortedArrays(nums1 = nums1, nums2 = nums2, method = "Kotlin collections") { pair ->
+        findMedianWithCollections(pair.first, pair.second)
     }
-
-    printLongestSubstring(strings = strings, method = "Sliding Window") { string ->
-        slidingWindow(string)
-    }
-
-    printLongestSubstring(strings = strings, method = "Sliding Window Optimized") { string ->
-        slidingWindowOptimized(string)
+    printMedianOfTwoSortedArrays(nums1 = nums1, nums2 = nums2, method = "Recursive Approach") { pair ->
+        findMedianSortedArrays(pair.first, pair.second)
     }
 
 }
 
-fun printLongestSubstring(strings: ArrayList<String>, method: String, block: (String) -> Int) {
-    strings.forEach {  string ->
-        val length = block(string)
-        println("Length of longest substring in $string is solved with $method is $length")
-    }
-}
-
-fun lengthOfLongestSubstring(s: String): Int {
-    val n = s.length
-    var ans = 0
-    for (i in 0 until n)
-        for (j in i + 1..n)
-            if (allUnique(s, i, j)) ans = Math.max(ans, j - i)
-    return ans
-}
-
-fun allUnique(s: String, start: Int, end: Int): Boolean {
-    val set = HashSet<Char>()
-    for (i in start until end) {
-        val ch = s[i]
-        if (set.contains(ch)) return false
-        set.add(ch)
-    }
-    return true
-}
-
-fun slidingWindow(s: String): Int {
-    val n = s.length
-    val set = HashSet<Char>()
-    var ans = 0
-    var i = 0
-    var j = 0
-    while (i < n && j < n) {
-        // try to extend the range [i, j]
-        if (!set.contains(s[j])) {
-            set.add(s[j++])
-            ans = Math.max(ans, j - i)
+fun printMedianOfTwoSortedArrays(nums1: IntArray, nums2: IntArray, method: String, block: (Pair<IntArray, IntArray>) -> Double) {
+    val numbers = block(Pair(nums1, nums2))
+    print("Median using $method method for numbers ")
+    nums1.forEachIndexed { index, i ->
+        print(i)
+        if (index != nums1.lastIndex) {
+            print(", ")
         } else {
-            set.remove(s[i++])
+            print(" and ")
         }
     }
-    return ans
+    nums2.forEachIndexed { index, i ->
+        print(i)
+        if (index != nums2.lastIndex) {
+            print(", ")
+        }
+    }
+    println(" is $numbers")
 }
 
-fun slidingWindowOptimized(s: String): Int {
-    val n = s.length
-    var ans = 0
-    val map = HashMap<Char, Int>() // current index of character
-    // try to extend the range [i, j]
-    var j = 0
-    var i = 0
-    while (j < n) {
-        if (map.containsKey(s[j])) {
-            i = Math.max(map[s[j]]!!, i)
-        }
-        ans = Math.max(ans, j - i + 1)
-        map[s[j]] = j + 1
-        j++
-    }
-    return ans
+
+fun findMedianWithCollections(nums1: IntArray, nums2: IntArray): Double {
+    val nums = mutableListOf<Double>()
+    nums1.forEach { nums.add(it.toDouble()) }
+    nums2.forEach { nums.add(it.toDouble()) }
+    return median(nums)
 }
+
+fun median(l: List<Double>) = l.sorted().let { (it[it.size / 2] + it[(it.size - 1) / 2]) / 2 }
+
+fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+    var A = nums1
+    var B = nums2
+    var m = A.size
+    var n = B.size
+    if (m > n) { // to ensure m<=n
+        val temp = A
+        A = B
+        B = temp
+        val tmp = m
+        m = n
+        n = tmp
+    }
+    var iMin = 0
+    var iMax = m
+    val halfLen = (m + n + 1) / 2
+    while (iMin <= iMax) {
+        val i = (iMin + iMax) / 2
+        val j = halfLen - i
+        if (i < iMax && B[j - 1] > A[i]) {
+            iMin = i + 1 // i is too small
+        } else if (i > iMin && A[i - 1] > B[j]) {
+            iMax = i - 1 // i is too big
+        } else { // i is perfect
+            var maxLeft = 0
+            maxLeft = when {
+                i == 0 -> B[j - 1]
+                j == 0 -> A[i - 1]
+                else -> Math.max(A[i - 1], B[j - 1])
+            }
+            if ((m + n) % 2 == 1) {
+                return maxLeft.toDouble()
+            }
+
+            var minRight = 0
+            minRight = when {
+                i == m -> B[j]
+                j == n -> A[i]
+                else -> Math.min(B[j], A[i])
+            }
+
+            return (maxLeft + minRight) / 2.0
+        }
+    }
+    return 0.0
+}
+
 
 
 
